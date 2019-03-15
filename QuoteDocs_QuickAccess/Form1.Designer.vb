beac7ac1a0ce360543a866e5c5080bf35c61530d
Charlie Boushek <Charlie@Aero-Material.com>
2019-03-14 23:41:35 -0500

    version label is now actually updated

--- QuoteDocs_QuickAccess/Form1.Designer.vb
+++ QuoteDocs_QuickAccess/Form1.Designer.vb
@@ -250,7 +250,7 @@ Partial Class Form1
         Me.lblRevision.Name = "lblRevision"
         Me.lblRevision.Size = New System.Drawing.Size(39, 15)
         Me.lblRevision.TabIndex = 23
-        Me.lblRevision.Text = "v4.0.0"
+        Me.lblRevision.Text = "v4.0.2"
         '
         'Form1
         '
